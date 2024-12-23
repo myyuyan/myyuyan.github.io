45fac4a87450de5ac861a170b0b9503cad3a3493
myuyan <510294009@qq.com>
2024-12-23 14:23:15 +0800

    yangming

---
+++ script.js
@@ -0,0 +1,8 @@
+const videos = document.querySelectorAll('.video');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) entry.target.play();
+        else entry.target.pause();
+      });
+    }, { threshold: 0.7 });
+    videos.forEach(v => observer.observe(v));
