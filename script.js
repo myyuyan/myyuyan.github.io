b40986d7cc6ffeae48764115c4699a127cb14929
myuyan <510294009@qq.com>
2024-12-23 15:06:33 +0800

    yangming

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const videos = document.querySelectorAll('.video');
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      const video = entry.target;
-      if (!video.src) {
-        video.src = video.dataset.src;
+      if (!entry.target.src) {
+        entry.target.src = entry.target.dataset.src;
       }
       entry.target.play();
     }else entry.target.pause();
